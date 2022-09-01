badcfde46088d89e63706dddbcb4c15bc041efbb
Sergey-la <bogdanetsserg@yandex.ru>
2022-09-01 21:50:52 +0300

    change slider

--- client/src/Components/Panel/Slider.js
+++ client/src/Components/Panel/Slider.js
@@ -1,45 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import 'swiper/css/bundle';
 
 export default function Slider() {
     const hotels = useSelector((store) => store.images);
 
     return (
-        <Swiper
-            slidesPerView={4}
-            breakpoints={{
-                // when window width is >= 640px
-                640: {
-                    width: 640,
-                    slidesPerView: 1,
-                },
-                // when window width is >= 768px
-                768: {
-                    width: 768,
-                    slidesPerView: 4,
-                },
-            }}
-        >
-            <SwiperSlide>
-                <img src={hotels.images[0]} alt="классная картинка" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={hotels.images[1]} alt="классная картинка" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={hotels.images[2]} alt="классная картинка" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={hotels.images[0]} alt="классная картинка" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={hotels.images[1]} alt="классная картинка" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={hotels.images[2]} alt="классная картинка" />
-            </SwiperSlide>
-        </Swiper>
+        <div className="slider">
+            <Swiper
+                slidesPerView={4}//брейкпоинты убрал, всё оставил как есть, в изначальном виде.
+            >
+                <SwiperSlide>
+                    <img src={hotels.images[0]} alt="классная картинка" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={hotels.images[1]} alt="классная картинка" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={hotels.images[2]} alt="классная картинка" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={hotels.images[0]} alt="классная картинка" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={hotels.images[1]} alt="классная картинка" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={hotels.images[2]} alt="классная картинка" />
+                </SwiperSlide>
+            </Swiper>
+        </div>
     );
 }
